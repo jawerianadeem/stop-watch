f8cb8a436f8948b0ca844441cf90bd5b2068651d
jawerianadeem <143826078+jawerianadeem@users.noreply.github.com>
2023-10-14 03:44:39 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+var min=0;
+var sec=0;
+var milisec=0;
+
+var minHeading=document.getElementById("min");
+var secHeading=document.getElementById("sec");
+var msecHeading=document.getElementById("milisec");
+
+var interval;
+function timer(){
+    milisec++;
+    msecHeading.innerHTML=milisec;
+
+
+
+if( milisec == 100){
+    sec++
+    secHeading.innerHTML=sec;
+    milisec=0;
+}  else if(sec >=60){
+    min++
+    sec=0
+    minHeading.innerHTML=min
+}
+}
+function start(){
+interval=setInterval(timer,10)}
+
+function stop(){
+    clearInterval(interval)
+}
+
+function reset(){
+    min=0;
+    sec=0;
+    milisec=0;
+    minHeading.innerHTML=min;
+    secHeading.innerHTML=sec;
+    msecHeading.innerHTML=milisec;
+    stop();
+}
